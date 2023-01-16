e32f93fbd92e6e224aa74b1f9be9588d2672ad7b
italo <italogithub@gmail.com>
2023-01-16 06:16:44 -0300

    update

--- Filas/main_fila.cpp
+++ Filas/main_fila.cpp
@@ -6,7 +6,30 @@ using namespace std;
 
 
 int main(){
-    fila fila1
+    fila fila1;
+    int op;
+    TipoItem item;
+    cout << "Programa gerador de filas\n";
+    do{
+        cout << "Digite 0 para parar o programa!\n";
+        cout << "Digite 1 para inserir um elemento: \n";
+        cout << "Digite 2 para remover um elemento: \n";
+        cout << "Digite 3 para imprimir a fila: \n";
+        cin >> op;
+        if (op == 1){
+            cout << "Digite o elemento a ser inserido na fila: ";
+            cin >> item;
+            fila1.inserir(item);
+        } else if (op == 2){
+            item = fila1.remover();
+            cout << "O elemento removido: " << item << endl;
+
+        } else if (op == 3){
+            fila1.imprimir();
+        } else{
+            cout << "Opção invalida!\n";
+        }
+    } while(op != 0);
 
 
 
