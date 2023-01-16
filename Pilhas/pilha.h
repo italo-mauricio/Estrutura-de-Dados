df56cd1a94b895b835dea58ef26b58d15a7bfda0
italo <italogithub@gmail.com>
2023-01-15 21:58:44 -0300

    update

--- Pilhas/pilha.h
+++ Pilhas/pilha.h
@@ -1,10 +1,17 @@
 // pilha = stack
 
-
+typedef int TipoItem;    // posso trocar pra float a qualquer momento no código sem mexer nas estruturas
+const int max_itens = 100;
 
 class pilha{
+
+    // somente a classe pode acessar
     private:
+    int tamanho;
+    int* estrutura;
 
 
+    // outros de fora podem acessar
     public:
+    pilha();   // função construtra tem que ser o mesmo nome da classe
 };
