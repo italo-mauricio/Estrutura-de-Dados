da532c52101326ef92bffca107cafc7cb6e046d6
italo <italogithub@gmail.com>
2023-01-15 22:49:52 -0300

    estruturando o main

---
+++ Pilhas/main_pilha.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "pilha.h"
+
+using namespace std;
+
+
+int main(){
+
+    pilha pilha1;
+    TipoItem item;
+    int op;
+    cout << "Programa gerador de pilhas\n";
+
+    do{
+        cout << "Digite 0 para parar o programa\n";
+        cout << "Digite 1 para inserir um elemento\n";
+        cout << "Digite 2 para remover um elemento\n";
+        cout << "Digite 3 para imprimir a pilha\n";
+        cout << "Digite 4 para verificar o tamanho da pilha\n";
+    }
+
+
+    return 0;
+}
