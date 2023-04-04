b936db9fb9aeb40de9055eba0457720166e27c72
italo <italogithub@gmail.com>
2023-04-04 18:03:37 -0300

    feat: working vector

---
+++ Estrutura-Java/src/com/italo/vetores/AppVetores.java
@@ -0,0 +1,7 @@
+package com.italo.vetores;
+
+public class AppVetores {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Estrutura-Java/src/com/italo/vetores/Vetor.java
@@ -0,0 +1,10 @@
+package com.italo.vetores;
+
+public class Vetor {
+    private String[] elementos;
+
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
